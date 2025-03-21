d5eda7de8ec968fab5b279a8d37edf899c41c4cf
Vaishali-Ajmera <vaishaliajmera11@gmail.com>
2025-03-21 14:20:46 +0530

    responsiveness done

--- src/Components/Header/RightImg.jsx
+++ src/Components/Header/RightImg.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 function RightImg() {
   return (
-    <>
-    <div>
-    <img className='h-15 w-30' src="/trio image.png" alt="trio img" />
-    </div></>
-  )
+    <div className="flex justify-center sm:justify-end">
+      <img 
+        className="h-auto w-[100px] sm:w-[70px] md:w-[100px] lg:w-[120px] xl:w-[150px] 
+                   object-contain transition-all duration-300" 
+        src="/trio image.png" 
+        alt="trio img" 
+      />
+    </div>
+  );
 }
 
-export default RightImg
+export default RightImg;
